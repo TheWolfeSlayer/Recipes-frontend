644ff025b028d5dd7f6f2954b7e8af6828e9a16e
Juan Mireles <119257668+Kal0yanX@users.noreply.github.com>
2023-07-16 10:05:42 -0400

    adding bootstrap to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,33 +2,43 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Home() {
-  const [recipes, setRecipes] = useState([])
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const URL = `${process.env.REACT_APP_BACKEND_URI}/recipes`
-      const response = await fetch(URL)
-      const data = await response.json()
-      setRecipes(data)
-    }
+      const URL = `${process.env.REACT_APP_BACKEND_URI}/recipes`;
+      const response = await fetch(URL);
+      const data = await response.json();
+      setRecipes(data);
+    };
 
-    fetchData()
-  }, [])
+    fetchData();
+  }, []);
 
-  const display = recipes.map(recipe => {
+  const display = recipes.map((recipe) => {
     return (
-      <div key={recipe._id}>
-        <Link to={`/recipes/${recipe._id}`}>
-          <p>{recipe.name}</p>
-        </Link>
+      <div key={recipe._id} className="col-lg-4 col-md-6 mb-4">
+        <div className="card" style={{ width: "100%" }}>
+          <img
+            src={recipe.image}
+            className="card-img-top"
+            alt={recipe.name}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{recipe.name}</h5>
+            <Link to={`/recipes/${recipe._id}`} className="btn btn-primary">
+              View Recipe
+            </Link>
+          </div>
+        </div>
       </div>
-    )
-  })
+    );
+  });
 
   return (
-    <div>
+    <div className="container">
       <h1>Recipe Database</h1>
-      {display}
+      <div className="row">{display}</div>
     </div>
   );
 }
