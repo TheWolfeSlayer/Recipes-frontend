bbff205b309a1953fb0065960975f1d56bd8241b
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-07-16 10:20:49 -0400

    steps now in ordered list

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -3,57 +3,59 @@ import { useParams, useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 
 function Recipe() {
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const [recipe, setRecipe] = useState(null)
+  const [recipe, setRecipe] = useState(null)
 
-    const { id } = useParams()
-    const URL = `${process.env.REACT_APP_BACKEND_URI}/recipes/${id}`
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            const response = await fetch(URL)
-            const data = await response.json()
-            setRecipe(data)
-        }
-
-        fetchData()
-    }, [id, URL])
-
-    const deleteRecipe = async () => {
-        const response = await fetch(URL, {
-            method: 'DELETE'
-        })
-        if (response.status !== 204) console.log('error') // add error handling later
-        navigate('/')
+  const { id } = useParams()
+  const URL = `${process.env.REACT_APP_BACKEND_URI}/recipes/${id}`
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(URL)
+      const data = await response.json()
+      setRecipe(data)
     }
 
-    const display = recipe && (
-        <div >
-            <h1>{recipe.name}</h1>
-            <img src={recipe.image} alt={recipe.name} height={300} />
-            <div>
-                <Button variant="success" onClick={() => navigate(`/recipes/update/${id}`)}>Edit</Button>
-                <Button variant="danger" onClick={deleteRecipe}>Delete</Button>
-            </div>
-            <h2>Ingredients Needed</h2>
-            <ul>
-                {recipe.ingredients.map((ingredient, index) => (
-                    <li key={index}>{ingredient}</li>
-                ))}
-            </ul>
-            <h2>Steps</h2>
-            {recipe.steps.map((step, index) => (
-                <p key={index}>{step}</p>
-            ))}
-        </div>
-    )
+    fetchData()
+  }, [id, URL])
 
-    return (
-        <div>
-            {display}
-        </div>
-    )
+  const deleteRecipe = async () => {
+    const response = await fetch(URL, {
+      method: 'DELETE'
+    })
+    if (response.status !== 204) console.log('error') // add error handling later
+    navigate('/')
+  }
+
+  const display = recipe && (
+    <div >
+      <h1>{recipe.name}</h1>
+      <img src={recipe.image} alt={recipe.name} height={300} />
+      <div>
+        <Button variant="success" onClick={() => navigate(`/recipes/update/${id}`)}>Edit</Button>
+        <Button variant="danger" onClick={deleteRecipe}>Delete</Button>
+      </div>
+      <h2>Ingredients Needed</h2>
+      <ul>
+        {recipe.ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <h2>Steps</h2>
+      <ol>
+        {recipe.steps.map((step, index) => (
+          <li key={index}>{step}</li>
+        ))}
+      </ol>
+    </div>
+  )
+
+  return (
+      <div>
+          {display}
+      </div>
+  )
 }
 
 export default Recipe
