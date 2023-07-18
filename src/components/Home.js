74b564a14a89e726522d837e85b27247651cb218
Matthew Wolfe <thewolfeslayer@gmail.com>
2023-07-18 18:54:34 -0400

    fixed imports

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
 
 function Home() {
   const [recipes, setRecipes] = useState([]);
